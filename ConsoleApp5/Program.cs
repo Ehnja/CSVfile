c4c45ba8296c8f3219d92147a53a56f5899a25cd
Hilmer Oudijk <hilmeroudijk@gmail.com>
2022-08-24 12:17:15 +0200

    third

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -10,48 +10,47 @@ namespace ConsoleApp5
         {
             try
             {
-
                 using (var reader = new StreamReader(@"C:\flyvning\flyvning1.csv"))
                 {
                     List<string> exportSVC = new List<string>();
-                    List<string[]> dataArrays = new List<string[]>();
-
-                    List<string> GPSLong = new List<string>();
-                    List<string> GPSLat = new List<string>();
+                    List<string>[] DataRest = new List<string>[14];
+                    for (int i = 0; i < DataRest.Length; i++)
+                    {
+                        DataRest[i] = new List<string>();
+                    }
 
                     while (!reader.EndOfStream)
                     {
                         string line = reader.ReadLine();
                         string[] values = line.Split(';');
-
-                        GPSLong.Add(values[2]);
-                        GPSLat.Add(values[3]);  
+                                              
+                        for (int i = 0; i < 13; i++)
+                        {
+                            DataRest[i].Add(values[i]);
+                        }
+                        
                     }
 
-                    for (int i = 2; i < GPSLong.Count - 1; i++)
+                    for (int i = 2; i < DataRest[2].Count - 1; i++)
                     {
-                        GPSLong[i] = GPSLong[i].Replace(".", string.Empty);
-                        GPSLong[i] = GPSLong[i].Insert(1, ".");
+                        DataRest[2][i] = DataRest[2][i].Replace(".", string.Empty);
+                        DataRest[2][i] = DataRest[2][i].Insert(1, ".");
                     }
-                    for (int i = 2; i < GPSLat.Count - 1; i++)
+                    for (int i = 2; i < DataRest[3].Count - 1; i++)
                     {
-                        GPSLat[i] = GPSLat[i].Replace(".", string.Empty);
-                        GPSLat[i] = GPSLat[i].Insert(2, ".");
-                    }
-                    for (int i = 2; i < GPSLat.Count; i++)
-                    {
-                        exportSVC.Add(GPSLong[i] + ", " + GPSLat[i]);
-
+                        DataRest[3][i] = DataRest[3][i].Replace(".", string.Empty);
+                        DataRest[3][i] = DataRest[3][i].Insert(2, ".");
                     }
 
-                    File.WriteAllLines(@"C:\flyvning\flyvning2.csv", exportSVC);
-
-                    for (int i = 0; i < GPSLong.Count - 1; i++)
-                    {
-                        Console.WriteLine(GPSLat[i]);
-                        Console.WriteLine(GPSLong[i]);
-                    }
+                    exportSVC.Add("Ticks; offsetTime; GPSLong; GPSLat; GPSDate; GPSTime; GPSheightMSL; GPShDOP; GPSpDOP; GPSsAcc; GPSnumGPS; GPSnumGLNAS; GPSnumSV; GPSkoord");
                     
+                    for (int i = 2; i < DataRest[2].Count; i++)
+                    {
+                        exportSVC.Add(DataRest[0][i] + ";" + DataRest[1][i] + ";" + DataRest[2][i] + ";" + DataRest[3][i] + ";" + DataRest[4][i] + ";" + DataRest[5][i] + ";" + DataRest[6][i] + ";" + DataRest[7][i] + ";" + DataRest[8][i] + ";" + DataRest[9][i] + ";" + DataRest[10][i] + ";" + DataRest[11][i] + ";" + DataRest[12][i] + ";" + DataRest[2][i] + ", " + DataRest[3][i]);
+
+                    }
+
+                    File.WriteAllLines(@"C:\flyvning\flyvning2.csv", exportSVC); 
                 }
             }
             catch (Exception e)
